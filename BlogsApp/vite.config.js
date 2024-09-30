100018a2ef531a964d5528495793ca584f93bc28
Kunal Ingale <112793345+Kunal-Ingale@users.noreply.github.com>
2024-09-30 18:55:20 +0530

    Update vite.config.js

--- BlogsApp/vite.config.js
+++ BlogsApp/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
    proxy:{
-    '/api':"http://localhost:5000"
+    '/api':"https://writer-s-whisper-blogs.vercel.app/"
    }
   },
   plugins: [react()],
